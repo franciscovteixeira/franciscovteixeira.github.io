d03f921a386389e851aaeccdb6f66a85c84cb4a0
Francisco Teixeira <francisco.v.teixeira@protonmail.com>
2026-04-15 15:21:49 -0300

    teste inicial de layout e estrutura do projeto.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+const projects = [
+  {
+    name: "Projeto 1",
+    description: "Descrição do projeto",
+    link: "#"
+  },
+  {
+    name: "Projeto 2",
+    description: "Outro projeto interessante",
+    link: "#"
+  }
+];
 
+const container = document.getElementById("projectList");
+
+projects.forEach(project => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <h3>${project.name}</h3>
+    <p>${project.description}</p>
+    <a class="btn" href="${project.link}" target="_blank">Ver Projeto</a>
+  `;
+
+  container.appendChild(card);
+});
